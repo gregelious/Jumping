7a0eb612c11c63ccf761416f04c1b79120e8d323
gregelious <90730434+gregelious@users.noreply.github.com>
2023-11-24 21:50:57 -0500

    imported scripts from class

--- Leearning to Jump/Assets/Scripts/Enemies.cs
+++ Leearning to Jump/Assets/Scripts/Enemies.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class Enemies : MonoBehaviour
 {
-
+    //variables for the game
     Health damage;
+
     public GameObject hitPoints;
+
+    // Vector2 direction;
 
     public float speed;
     public bool left = true;
@@ -24,10 +27,13 @@ public class Enemies : MonoBehaviour
         {
             transform.position = new Vector2(transform.position.x - speed, transform.position.y);
         }
+
         else
         {
             transform.position = new Vector2(transform.position.x + speed, transform.position.y);
         }
+
+
     }
 
     void OnTriggerEnter2D(Collider2D coll)
@@ -36,11 +42,13 @@ public class Enemies : MonoBehaviour
         {
             damage.TakeDamage(1);
         }
+
         if (coll.tag == "Wall" && left == true)
         {
             left = false;
         }
-        else if ( coll.tag == "Wall" && left == false)
+
+        else if (coll.tag == "Wall" && left == false)
         {
             left = true;
         }
--- Leearning to Jump/Assets/Scripts/Health.cs
+++ Leearning to Jump/Assets/Scripts/Health.cs
@@ -5,56 +5,37 @@ using UnityEngine.SceneManagement;
 
 public class Health : MonoBehaviour
 {
+    //variable for the gamr
+    public int maxHealth;       //determines max amount of health the player can have
+    public int currentHealth;   //tracks current health of the player
 
-    public int maxHealth;
-    public int currentHealth;
+    public GameObject hp1;
+    public GameObject hp2;
 
-    public Transform startPos;
-
-    public GameObject health1;
-    public GameObject health2;
 
     // Start is called before the first frame update
     void Start()
     {
-        maxHealth = 3;                  //set max health to two
+        maxHealth = 2;                  //set max health to two
         currentHealth = maxHealth;      //set current health to max health
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    void OnCollisionEnter2D(Collision2D other)
-    {
-        if (other.gameObject.CompareTag("Respawn"))
-        {
-            TakeDamage(1);
-        }
     }
 
     public void TakeDamage(int amount)
     {
         currentHealth -= amount;                        //lower health
-        transform.position = new Vector2(startPos.position.x, startPos.position.y);
 
         if (currentHealth == 2)
         {
-            health2.SetActive(false);
+            hp2.SetActive(false);
         }
-
         if (currentHealth == 1)
         {
-            health1.SetActive(false);
+            hp1.SetActive(false);
         }
-
 
         if (currentHealth <= 0)                         //if health equal zero
         {
             SceneManager.LoadScene("GameOver");         //go to GameOver Scene
-
         }
     }
 }
